f43259d90e8646e03ae4b09ef1e70bc84afa02ba
Graham Towse <1694123+heycomputer@users.noreply.github.com>
2024-03-03 09:55:46 +1100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,44 @@
+import { Command, Options } from "@effect/cli";
+import { Config, ConfigProvider, Console, Effect, Layer, Option } from "effect";
+import { NodeContext, NodeRuntime } from "@effect/platform-node";
+
+const spaceId = Options.text("space-id").pipe(
+  Options.withAlias("s"),
+  Options.withFallbackConfig(Config.string("SPACE_ID"))
+);
+
+const environmentId = Options.text("environment-id").pipe(
+  Options.withAlias("e"),
+  Options.withFallbackConfig(Config.string("ENVIRONMENT_ID"))
+);
+
+const accessToken = Options.secret("access-token").pipe(
+  Options.withAlias("t"),
+  Options.withFallbackConfig(Config.secret("ACCESS_TOKEN"))
+);
+
+// legion [--version] [-h | --help] [-c SPACE_ID] [-e ENVIRONMENT_ID] [-t ACCESS_TOKEN]
+const legion = Command.make(
+  "legion",
+  { spaceId, environmentId, accessToken },
+  ({ spaceId, environmentId }) =>
+    Console.log(
+      [
+        "running legion",
+        `space-id: ${spaceId}`,
+        `environment-id: ${environmentId}`,
+      ].join("\n")
+    )
+);
+
+const command = legion;
+
+const cli = Command.run(command, {
+  name: "Legion Contentful CLI",
+  version: "v0.0.1",
+});
+
+Effect.suspend(() => cli(process.argv)).pipe(
+  Effect.provide(NodeContext.layer),
+  NodeRuntime.runMain
+);
