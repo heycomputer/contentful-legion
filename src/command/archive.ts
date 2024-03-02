3b21b01c0280fc561380478b1e034297f0fa9a00
Graham Towse <1694123+heycomputer@users.noreply.github.com>
2024-03-03 10:29:35 +1100

    Create archive.ts

---
+++ src/command/archive.ts
@@ -0,0 +1,8 @@
+import { Command, Options } from "@effect/cli";
+import { Console } from "effect";
+
+const query = Options.text("query").pipe(Options.withAlias("q"));
+
+const archive = Command.make("archive", { query }, ({ query }) => {
+  return Console.log(`Running 'legion archive ${query}'`);
+});
