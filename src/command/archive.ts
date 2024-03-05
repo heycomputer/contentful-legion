e0fd432fd56d800dc5568c3270bf4e2a06750d1c
Graham Towse <1694123+heycomputer@users.noreply.github.com>
2024-03-05 11:06:03 +1100

    Add choices for entity type (asset or entry)

--- src/command/archive.ts
+++ src/command/archive.ts
@@ -1,8 +1,27 @@
 import { Args, Command, Options } from "@effect/cli";
 import { Console } from "effect";
+import * as Data from "effect/Data";
+
+type Entity = Entry | Asset;
+class Entry extends Data.TaggedClass("Entry")<{}> {}
+class Asset extends Data.TaggedClass("Asset")<{}> {}
+const asset = new Asset();
+const entry = new Entry();
+
+const entity = Args.choice<Entity>(
+  [
+    ["asset", asset],
+    ["entry", entry],
+  ],
+  { name: "entity" }
+);
 
 const query = Args.text({ name: "query" });
 
-export const archive = Command.make("archive", { query }, ({ query }) => {
-  return Console.log(`Running 'legion archive ${query}'`);
-});
+export const archive = Command.make(
+  "archive",
+  { entity, query },
+  ({ entity, query }) => {
+    return Console.log(`Running 'legion archive ${entity} ${query}'`);
+  }
+).pipe(Command.withDescription("archive entities that match a query"));
